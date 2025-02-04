1f1bc4b97c3b78702d3da4b0e95c617aca80b8e8
JacobProut <Jprout@wgu.edu>
2025-02-03 23:46:21 -0600

    Employees Possibly Completed

--- src/Models/Employees.java
+++ src/Models/Employees.java
@@ -10,13 +10,109 @@ public class Employees {
     private int countryId;
     private int storeId;
 
+
     protected String employeeFirstName;
     protected String employeeLastName;
     private String employeeAddress;
     private String employeePostalCode;
     private String employeePhoneNumber;
-    private String createdBy;
-    private String lastUpdatedBy;
-    private LocalDateTime lastUpdated;
     private LocalDateTime createDate;
+    private String createdBy;
+    private LocalDateTime lastUpdated;
+    private String lastUpdatedBy;
+
+
+    public Employees(int employeeId, String employeeFirstName, String employeeLastName, String employeeAddress, String employeePostalCode, String employeePhoneNumber, LocalDateTime createDate, String createdBy, LocalDateTime lastUpdated, String lastUpdatedBy, int divisionId, int countryId, int storeId) {
+        this.employeeId = employeeId;
+        this.employeeFirstName = employeeFirstName;
+        this.employeeLastName = employeeLastName;
+        this.employeeAddress = employeeAddress;
+        this.employeePostalCode = employeePostalCode;
+        this.employeePhoneNumber = employeePhoneNumber;
+        this.createDate = createDate;
+        this.createdBy = createdBy;
+        this.lastUpdated = lastUpdated;
+        this.lastUpdatedBy = lastUpdatedBy;
+        this.divisionId = divisionId;
+        this.countryId = countryId;
+        this.storeId = storeId;
+    }
+
+    //Will be used for EmployeeDAO.getAllEmployeesById(int)
+    public Employees(int collectedId, String collectedName) {
+        this.employeeId = collectedId;
+        this.employeeFirstName = collectedName;
+    }
+
+    public int getDivisionId() {
+        return divisionId;
+    }
+    public void setDivisionId(int divisionId) {
+        this.divisionId = divisionId;
+    }
+    public int getCountryId() {
+        return countryId;
+    }
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+    public int getStoreId() {
+        return storeId;
+    }
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+    public int getEmployeeId() {
+        return employeeId;
+    }
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+
+    public String getEmployeeFirstName() {
+        return employeeFirstName;
+    }
+    public String getEmployeeLastName() {
+        return employeeLastName;
+    }
+    public String getEmployeeAddress() {
+        return employeeAddress;
+    }
+    public String getEmployeePostalCode() {
+        return employeePostalCode;
+    }
+    public String getEmployeePhoneNumber() {
+        return employeePhoneNumber;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+    public String getCreatedBy() {
+        return createdBy;
+    }
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+    public LocalDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+    public void setLastUpdated(LocalDateTime lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+    public String getLastUpdatedBy() {
+        return lastUpdatedBy;
+    }
+    public void setLastUpdatedBy(String lastUpdatedBy) {
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+
+    public String toString() {
+        return this.employeeFirstName;
+    }
+
 }
